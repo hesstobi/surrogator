faf65da5e19d09333682c7aafc741296e06804ac
Tobias Hess <me@tobiashess.de>
2016-02-28 22:37:13 +0100

    Fix lowercase email error

--- www/reset.php
+++ www/reset.php
@@ -6,26 +6,28 @@ require '../vendor/autoload.php';
 
 $cfgFile = __DIR__ . '/../data/surrogator.config.php';
 if (!file_exists($cfgFile)) {
-    $cfgFile = '/etc/surrogator.config.php';
-    if (!file_exists($cfgFile)) {
-        err(
-            500,
-            "Configuration file does not exist.",
-            "Copy data/surrogator.config.php.dist to data/surrogator.config.php"
-        );
-        exit(2);
-    }
+	$cfgFile = '/etc/surrogator.config.php';
+	if (!file_exists($cfgFile)) {
+		err(
+			500,
+			"Configuration file does not exist.",
+			"Copy data/surrogator.config.php.dist to data/surrogator.config.php"
+		);
+		exit(2);
+	}
 }
 require $cfgFile;
 require __DIR__ . '/shib_attr.php';
 
-list($md5, $sha256) = getHashes($shib_mail);
+$mail = strtolower($shib_mail);
+
+list($md5, $sha256) = getHashes($mail);
 
 shell_exec('rm -r ' . $varDir . '*/' . $md5 . '.*');
 shell_exec('rm -r ' . $varDir . '*/' . $sha256 . '.*');
-shell_exec('rm ' . $varDir . 'square/' . $shib_mail . '.*');
-shell_exec('rm ' . $rawDir . $shib_mail . '.*');
-shell_exec('git -C ' . $rawDir . ' checkout -- ' . $shib_mail . '.*');
+shell_exec('rm ' . $varDir . 'square/' . $mail . '.*');
+shell_exec('rm ' . $rawDir . $mail . '.*');
+shell_exec('git -C ' . $rawDir . ' checkout -- ' . $mail . '.*');
 shell_exec('php ' . __DIR__ . '/../surrogator.php ');
 
 $results = array(
@@ -36,9 +38,9 @@ echo json_encode($results);
 
 function getHashes($emailAddress)
 {
-    return array(
-        md5($emailAddress), hash('sha256', $emailAddress)
-    );
+	return array(
+		md5($emailAddress), hash('sha256', $emailAddress)
+	);
 }
 
 
--- www/upload.php
+++ www/upload.php
@@ -6,15 +6,15 @@ require '../vendor/autoload.php';
 
 $cfgFile = __DIR__ . '/../data/surrogator.config.php';
 if (!file_exists($cfgFile)) {
-    $cfgFile = '/etc/surrogator.config.php';
-    if (!file_exists($cfgFile)) {
-        err(
-            500,
-            "Configuration file does not exist.",
-            "Copy data/surrogator.config.php.dist to data/surrogator.config.php"
-        );
-        exit(2);
-    }
+	$cfgFile = '/etc/surrogator.config.php';
+	if (!file_exists($cfgFile)) {
+		err(
+			500,
+			"Configuration file does not exist.",
+			"Copy data/surrogator.config.php.dist to data/surrogator.config.php"
+		);
+		exit(2);
+	}
 }
 require $cfgFile;
 require __DIR__ . '/shib_attr.php';
@@ -23,37 +23,38 @@ $storage = new \Upload\Storage\FileSystem($rawDir,true);
 $file = new \Upload\File('profileFile', $storage);
 
 // Optionally you can rename the file on upload
+$mail = strtolower($shib_mail);
 
-$file->setName($shib_mail);
+$file->setName($mail);
 
 // Validate file upload
 // MimeType List => http://www.iana.org/assignments/media-types/media-types.xhtml
 $file->addValidations(array(
-    // Ensure file is of type "image/png or jpeg"
-    new \Upload\Validation\Mimetype(array('image/png', 'image/jpeg')),
-    new \Upload\Validation\Size('5M')
+	// Ensure file is of type "image/png or jpeg"
+	new \Upload\Validation\Mimetype(array('image/png', 'image/jpeg')),
+	new \Upload\Validation\Size('5M')
 ));
 
 // Access data about the file that has been uploaded
 $data = array(
-    'name'       => $file->getNameWithExtension(),
-    'extension'  => $file->getExtension(),
-    'mime'       => $file->getMimetype(),
-    'size'       => $file->getSize(),
-    'md5'        => $file->getMd5(),
-    'dimensions' => $file->getDimensions()
+	'name'       => $file->getNameWithExtension(),
+	'extension'  => $file->getExtension(),
+	'mime'       => $file->getMimetype(),
+	'size'       => $file->getSize(),
+	'md5'        => $file->getMd5(),
+	'dimensions' => $file->getDimensions()
 );
 
 // Try to upload file
 try {
-    // Success!
-    shell_exec('rm ' . __DIR__ . '/../raw/' . $shib_mail . '.*');
-    $file->upload();
+	// Success!
+	shell_exec('rm ' . __DIR__ . '/../raw/' . $mail . '.*');
+	$file->upload();
 	$output = shell_exec('php ' . __DIR__ . '/../surrogator.php');
 
 	$results = array(
-    	'status'  => 'ok',
-    	'file_data' => $data,
+		'status'  => 'ok',
+		'file_data' => $data,
 		'surroggator' => $output
 	);
 
@@ -61,12 +62,12 @@ try {
 
 
 } catch (\Exception $e) {
-    // Fail!
-    $errors = $file->getErrors();
+	// Fail!
+	$errors = $file->getErrors();
 
 	$results = array(
-    	'status'  => 'error',
-    	'error' => $errors
+		'status'  => 'error',
+		'error' => $errors
 	);
 
 	echo json_encode($results);
