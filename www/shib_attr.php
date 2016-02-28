47222f8f49d368b404a11820d045fbf392c9eb87
Tobias Hess <me@tobiashess.de>
2016-02-28 22:38:53 +0100

    Change Atrributes

--- www/shib_attr.php
+++ www/shib_attr.php
@@ -5,7 +5,9 @@ $shib_provider = $_SERVER["Shib-Identity-Provider"] ?: "None";
 $shib_id =  $_SERVER["Shib-Application-ID"] ?: "None";
 $shib_session = $_SERVER["Shib-Session-ID"] ?: "None";
 $shib_eppn =   $_SERVER["eppn"] ?: "muster";
-$shib_cn =  $_SERVER["cn"] ?: "Max Mustermann";
+$shib_sn =  $_SERVER["sn"] ?: "Mustermann";
+$shib_givenName =  $_SERVER["givenName"] ?: "Max";
+$shib_displayName =  $_SERVER["displayName"] ?: "Max Mustermann";
 $shib_mail =   $_SERVER["mail"] ?: "max.mustermann@tu-dresden.de";
 $shib_affiliation =   $_SERVER["affiliation"] ?: "member@tu-dresden.de";
 
